1b810599376a59061967a3144d77ae5cc44b6089
Juanszf <juan_contec@hotmail.com>
2021-08-29 22:05:23 -0300

    Atualizando os exercícios

---
+++ Exercicios/exercicio24.py
@@ -0,0 +1,3 @@
+cidade = input('Qual o nome da sua cidade?\n')
+
+#não consegui pensar na resolução ainda
---
+++ Exercicios/exercicio25.py
@@ -0,0 +1,7 @@
+nome = input('Qual o seu nome completo?\n')
+capnome = nome.title()
+
+if 'Silva' in capnome:
+    print('\nVocê possui Silva em seu nome')
+else:
+    print('\nVocê não possui Silva em seu nome')
