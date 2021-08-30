f9d633fe8ef1d11202040e8b8b4e67eeb5201773
Juanszf <juan_contec@hotmail.com>
2021-08-29 21:28:37 -0300

    Create exercicio23.py

---
+++ Exercicios/exercicio23.py
@@ -0,0 +1,18 @@
+num = (input('Digite um número de 0 até 9999\n'))
+y=-1
+if num.isnumeric():
+    if (int(num)>= 0 and int(num) <= 9999):
+        for x in num:
+            if y == -1:
+                print(f'\nUnidade: {num[y]}')
+            elif y == -2:
+                print(f'Dezena: {num[y]}')
+            elif y == -3:
+                print(f'Centena: {num[y]}')
+            elif y == -4:
+                print(f'Milhar: {num[y]}')
+            y=y-1
+    else:
+        print('Você digitou um número inválido')
+else:
+    print('Você digitou uma texto não numérico')
